fecc2fbb53933b1ce61647c0bfed0818b0fe14d9
korewaChino <cappy@cappuchino.xyz>
2021-10-18 10:53:58 +0700

    customized installation

--- fedora.sh
+++ fedora.sh
@@ -1,119 +1,243 @@
 #!/usr/bin/env bash
+nomusic=false
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -n|--nomusic)
+	  nomusic=true
+	  echo -e "\033[0;36mINFO\033[0m: Music packages are disabled. Fedora Jam+ Will not be installed."
+      shift # past argument
+      ;;
+  esac
+done
+#fancy stuff
+
+spinner()
+{
+	local args=">> $*"
+	local count="${#args}"
+    local pid=$!
+    local delay=0.1
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf "[\033[0;34m%c\033[0m] %s" "$spinstr" "$args"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b\b\b"
+		for i in $(seq $count); do
+		printf "\b"
+		done
+    done
+    printf "   \b\b\b\b\b"
+}
+
+#Dependencies funny lol
+if ! command -v figlet &> /dev/null
+then
+    sudo dnf install figlet -yq
+fi
+
+clear
+figlet UML-Lite
+echo "Ultramarine Environment deployment script for Fedora"
 
 PACKAGE_LIST=(
-	vim
-	calibre
 	zsh
-	fira-code-fonts
+	fish
+	cascadia-code-fonts
+	cascadia-code-pl-fonts
 	lutris
 	steam
-	legendary
 	vlc
-	mcomix3
-	qbittorrent
 	htop
+	bpytop
 	gnome-boxes
 	handbrake
 	gnome-extensions-app
 	gnome-tweaks
-	gnome-shell-extension-pop-shell
 	python3
 	python3-pip
-	youtube-dl
 	neofetch
 	pv
 	wget
+	yabridge
 	java-latest-openjdk
 	java-11-openjdk
-	wine
 	heroic-games-launcher-bin
-	discord
 	linux-util-user
-	alacritty
 	python3-pip
+	kernel-xanmod-edge
 	fwupd
-	radeontop
-	
+	virt-manager
+	gnome-boxes
+	v4l2loopback
+	zerotier-one
+	gnome-shell-extension-mediacontrols
+	gnome-shell-extension-dash-to-dock
+	gnome-shell-extension-appindicator
+	gnome-shell-extension-places-menu
+	gnome-shell-extension-emoji-selector
+	gnome-shell-extension-user-theme
+	gnome-shell-extension-workspace-indicator
 )
 
 FLATPAK_LIST=(
-	io.lbry.lbry-app
 	org.telegram.desktop
-	com.mojang.Minecraft
-	net.veloren.airshipper
+
 )
+if [ -f .stage3 ]; then
+	figlet STAGE 3
+	sudo echo "Root Check!"
+	echo ">> You're almost done!"
+	echo ">> Installing extra packages..."
 
-# gnome settings
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
-
-# enable rpmfusion
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -yq
-
-sudo dnf groupupdate core -yq
-
-# install development tools 
-sudo dnf groupinstall "Development Tools" -yq
-
-# install multimedia packages
-sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -yq
-
-sudo dnf groupupdate sound-and-video -yq
-
-# fedora better fonts
-sudo dnf copr enable dawid/better_fonts -yq
-sudo dnf install fontconfig-enhanced-defaults fontconfig-font-replacements -yq
-
-# add flathub repository
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# add third party software
-  
-# add heroic games launcher
-sudo dnf copr enable atim/heroic-games-launcher -y
- 
-# update repositories
-
-sudo dnf check-update -yq
-
-# iterate through packages and installs them if not already installed
-for package_name in ${PACKAGE_LIST[@]}; do
-	if ! sudo dnf list --installed | grep -q "^\<$package_name\>"; then
-		echo "installing $package_name..."
-		sleep .5
-		sudo dnf install "$package_name" -y
-		echo "$package_name installed"
+	#FJ+ setup
+	if [ $nomusic = true ]; then
+	{
+		sudo dnf copr enable cappyishihara/fedorajam-plus
+		sudo dnf --disablerepo fedora --disablerepo updates install wine yabridge
+	} &> /dev/null & spinner "Setting up Fedora Jam+..."
+	echo ">> Setting up Fedora Jam+...done!"
 	else
-		echo "$package_name already installed"
+	echo -e "\033[0;36mINFO\033[0m: Music packages are disabled. Fedora Jam+ Will not be installed."
 	fi
-done
-
-for flatpak_name in ${FLATPAK_LIST[@]}; do
-	if ! flatpak list | grep -q $flatpak_name; then
-		flatpak install "$flatpak_name" -y
-	else
-		echo "$package_name already installed"
-	fi
-done
 
 
-# add ohmyzsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	#discord setup
+	pushd rpmbuild/discord &> /dev/null || return 127
+	yes | ./create-package.sh canary & spinner Installing Discord Canary...
+	echo ">> Installing Discord Canary...done!"
+	popd &> /dev/null || return 127
 
-# add protonup (now that prerequisites are fulfilled)
-pip install protonup
+	#OBS setup
 
-# add Mullvad
-wget --content-disposition https://mullvad.net/download/app/rpm/latest
+	pushd rpmbuild/obs-studio.spec &> /dev/null || return 127 
+	sudo dnf builddep obs-studio.spec -yq
+	{
+		./build.sh
+		cp f_downloads/* ~/rpmbuild/SOURCES/
+		rpmbuild -bb obs-studio.spec
+	} &> /dev/null & spinner Compiling OBS Studio + CEF Browser source...
+	pushd ~/rpmbuild/RPMS/x86_64/ &> /dev/null || exit
+	sudo dnf install ./obs* -y
+	popd &> /dev/null || exit
+	echo ">> Installing OBS Studio + CEF Browser source...done!"
+	popd &> /dev/null || return 127
 
-# add mesa-aco from GloriousEggroll
-sudo dnf copr enable gloriouseggroll/mesa-aco -yq 
 
-# upgrade packages
-sudo dnf distro-sync -y && sudo dnf update --refresh -y && flatpak update -y && flatpak remove --unused && sudo fwupdmgr get-updates
-sudo dnf autoremove -yq
+	rm ~/.config/autostart/postinstall.desktop
+	rm .stage3
+	echo ">> Configuration done!"
 
-echo "************************************************"
-echo "All good to go! Feel free to reboot your machine!"
-echo "************************************************"
-sleep 10
+elif [ ! -f .stage2 ]; then
+
+	# gnome settings
+	wget -qO- https://raw.githubusercontent.com/Bonandry/adwaita-plus/master/install.sh | sh
+	gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
+	gsettings set org.gnome.desktop.interface icon-theme "Adwaita++-Dark-Colorful"
+	sed "s/changeme/$USER/g" gdm-custom.conf > custom.conf
+	sudo cp custom.conf /etc/gdm/custom.conf
+	rm custom.conf
+
+	# enable rpmfusion
+	sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -yq & spinner "Installing RPMFusion Repos..."
+
+	
+	sudo dnf groupupdate core -yq & spinner Updating core packages...
+
+	# install development tools
+	
+	{
+		sudo dnf groupinstall "Development Tools" -yq 
+		sudo dnf groupinstall "RPM Development Tools" -yq
+	} & spinner Installing dev tools...
+	echo ">> Installing dev tools...done!"
+
+	{
+		sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -yq
+
+		sudo dnf groupupdate sound-and-video -yq
+	} & spinner Updating multimedia packages...
+	echo ">> Updating multimedia packages...done!"
+	# fedora better fonts
+	{
+		sudo dnf copr enable dawid/better_fonts -yq &> /dev/null
+		sudo dnf install fontconfig-enhanced-defaults fontconfig-font-replacements -yq
+	} & spinner Setting up Better fonts...
+	echo ">> Setting up Better fonts...done!"
+	# add flathub repository
+	sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+	# add third party software
+	
+	echo ">> Adding COPR Repos..."
+	{
+		sudo dnf copr enable sentry/v4l2loopback -yq
+		sudo dnf copr enable atim/heroic-games-launcher -yq
+	} &> /dev/null
+	printf "\bdone!"
+	# update repositories
+
+	sudo dnf check-update -yq
+
+	echo "-----------PACKAGE SETUP----------------"
+	# iterate through packages and installs them if not already installed
+	for package_name in ${PACKAGE_LIST[@]}; do
+		if ! sudo dnf list --installed | grep -q "^\<$package_name\>"; then
+			echo "installing $package_name..."
+			sleep .5
+			sudo dnf install "$package_name" -y &> /dev/null & spinner "Installing $package_name..."
+			echo "Installing $package_name...done!"
+		else
+			echo "$package_name already installed!"
+		fi
+	done
+
+	for flatpak_name in ${FLATPAK_LIST[@]}; do
+		if ! flatpak list | grep -q $flatpak_name; then
+			flatpak install "$flatpak_name" -y & spinner
+		else
+			echo "$package_name already installed"
+		fi
+	done
+	echo "-----------PACKAGE SETUP DONE-----------"
+	sudo usermod -aG mock,qemu,kvm $USER
+
+	# add protonup (now that prerequisites are fulfilled)
+	pip install protonup
+
+
+	# add mesa-aco from GloriousEggroll
+	sudo dnf copr enable gloriouseggroll/mesa-aco -yq &> /dev/null & spinner Adding mesa-aco...
+
+	# upgrade packages
+	sudo dnf distro-sync -y && sudo dnf update --refresh -y && flatpak update -y && flatpak remove --unused && sudo fwupdmgr get-updates & spinner Updating system...
+	sudo dnf autoremove -yq & spinner Cleaning up packages...
+	#set default kernel to xanmod
+	#sudo grubby --set-default $(ls /boot | grep vmlinuz | grep xm)
+
+	#prepare for stage 2
+	touch .stage2
+	sed "s|changeme|$PWD/fedora.sh|g" postinstall.desktop | sed "s|chme|$PWD|g" > ~/.config/autostart/postinstall.desktop
+
+	echo "************************************************"
+	echo "Rebooting machine in 10 seconds..."
+	echo "************************************************"
+	sleep 10
+	reboot
+else
+	figlet STAGE 2
+	echo ">> Installing NVIDIA Drivers..."
+	sudo dnf install akmod-nvidia -y
+	echo ">> Installing dotfiles..."
+	cp -R dotfiles/.config ~/.config
+	sed -i "s|changeme|$USER|g" ~/.config/neofetch/config.conf
+	mv .stage2 .stage3
+	echo "************************************************"
+	echo "Rebooting machine in 10 seconds..."
+	echo "************************************************"
+	sleep 10
+	reboot
+fi
+
